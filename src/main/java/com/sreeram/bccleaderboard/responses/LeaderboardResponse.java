40d8be64e591b3d17514fc52f90625d94fb3166b
Sreeram K <sreeramfireball@gmail.com>
2023-07-24 00:44:12 +0530

    added proper ranking criteria and did a refactor

--- src/main/java/com/sreeram/bccleaderboard/responses/LeaderboardResponse.java
+++ src/main/java/com/sreeram/bccleaderboard/responses/LeaderboardResponse.java
@@ -3,31 +3,28 @@ package com.sreeram.bccleaderboard.responses;
 import com.sreeram.bccleaderboard.models.Player;
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Getter
 public class LeaderboardResponse {
   
-  private List<PlayerResponse> topPlayers;
+  private List<Player> topPlayers;
 
   /**
    * prepares Leaderboard response
-   * @param players map containing the player username and the aggregate clubscore over
-   *                all tournaments started and finished current month
+   * @param players list of player objects
    */
-  public LeaderboardResponse(Map<String, Player> players) {
-    if(players == null) {
-      System.out.println("Players is null");
-      return;
-    }
-    topPlayers = new ArrayList<>();
-    for(Map.Entry<String, Player> entry : players.entrySet()) {
-      Player player = entry.getValue();
-      topPlayers.add(new PlayerResponse(entry.getKey(), player.getClubScoreAggregate()));
-    }
-    topPlayers.sort(((o1, o2) -> o2.getMonthScore() - o1.getMonthScore()));
+  public LeaderboardResponse(List<Player> players) {
+    topPlayers = players;
+    topPlayers.sort((p1, p2) -> {
+      if (p1.getClubScoreMonthAggregate().equals(p2.getClubScoreMonthAggregate())) {
+        if (p1.getTournamentPointsMonthAggregate().equals(p2.getTiebreakPointsMonthAggregate())) {
+          return Float.compare(p2.getTiebreakPointsMonthAggregate(), p1.getTiebreakPointsMonthAggregate());
+        }
+        return Float.compare(p2.getTournamentPointsMonthAggregate(), p1.getTournamentPointsMonthAggregate());
+      }
+      return Integer.compare(p2.getClubScoreMonthAggregate(), p1.getClubScoreMonthAggregate());
+    });
   }
 
 }
--- src/main/java/com/sreeram/bccleaderboard/responses/PlayerResponse.java
+++
@@ -1,9 +0,0 @@
-package com.sreeram.bccleaderboard.responses;
-
-import lombok.Data;
-
-@Data
-public class PlayerResponse {
-    private final String username;
-    private final Integer monthScore;
-}
